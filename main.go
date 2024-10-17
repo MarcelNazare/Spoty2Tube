d0ca572c1c98d8aef0160920bd1c64726f13b2f7
Marcel Nazare <realmarcelnazare@gmail.com>
2024-10-17 02:01:44 +0200

    lets work on the basic functionality

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import(
 	"fmt"
+	/*
 	"github.com/zmb3/spotify"
 	"github.com/kkdai/youtube"
 	"golang.org/x/oauth2"
 	"golang.org/x/net/context"
+	*/
 )
 
+func main() {
+	fmt.Println("Hello, World!")
+	}
